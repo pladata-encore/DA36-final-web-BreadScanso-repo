721f59705dcdb7bcaf07c3cdddd171847444b671
jjungah <negmhi29@gmail.com>
2025-02-07 18:48:48 +0900

    Rename: 기초 App 재공사

--- breadscanco/member/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# view.py 사용자의 요청을 처리하는 모듈
-def index(request):
-    print('index 함수 호출!')
-    return HttpResponse("Hello world! This is Django login🎀")
