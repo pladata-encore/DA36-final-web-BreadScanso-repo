e080bd412ad9e93680c4f9303b8e5c9a37351092
jjungah <negmhi29@gmail.com>
2025-02-10 12:48:05 +0900

    Update: setting, bs_urls, kiosk_view 수정

--- breadscanco/settings.py
+++ breadscanco/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+print("BASE_DIR = ", BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.humanize',  # template의 intcomma 활성화
+    'django.contrib.humanize',  # template의 intcomma 활성화,
+    'kiosk',
 ]
 
 MIDDLEWARE = [
