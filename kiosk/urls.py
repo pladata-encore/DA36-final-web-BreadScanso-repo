96ead2959e957589dc01476dc531d7f89d772a13
playdata\playdata2 <yejin3388@naver.com>
2025-02-10 19:38:37 +0900

    Update: kiosk 화면구성(main, products) #32

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  # views.py에서 함수 가져오기
 
 urlpatterns = [
-    path('', views.kiosk_main, name='kiosk_main'),  # /kiosk/
+    path('', views.kiosk_main, name='kiosk_main'),  # /kiosk/ kiosk 메인 페이지
+    path('products/', views.products, name='products'),  # 상품 안내 페이지 추가
 ]
--- kiosk/views.py
+++ kiosk/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def kiosk_main(request):
-    return render(request, '../templates/kiosk/kiosk_main.html')  # 템플릿 파일 경로 지정
+    return render(request, '../templates/kiosk/kiosk_main.html')  # kiosk_main 템플릿 파일 경로 지정
+
+def products(request):
+    return render(request, '../templates/kiosk/kiosk_products.html')  # kiosk_상품안내화면 템플릿 파일 경로 지정
+
