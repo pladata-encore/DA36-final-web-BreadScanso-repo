91d33611e64bc0a9f46e6f89d0ff2e65711e9e1e
playdata\playdata2 <yejin3388@naver.com>
2025-02-11 16:02:38 +0900

    Update: kiosk 회원 확인 -> 포인트 사용

--- kiosk/views.py
+++ kiosk/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 def kiosk_main(request):
-    return render(request, '../templates/kiosk/kiosk_main.html')  # kiosk_main 템플릿 파일 경로 지정
+    return render(request, 'kiosk/kiosk_main.html')  # kiosk_main 템플릿 파일 경로 지정
 
 def products(request):
-    return render(request, '../templates/kiosk/kiosk_products.html')  # kiosk_상품안내화면 템플릿 파일 경로 지정
+    return render(request, 'kiosk/kiosk_products.html')  # kiosk_상품안내화면 템플릿 파일 경로 지정
 
 def member(request):
-    return render(request, '../templates/kiosk/kiosk_member.html')  # kiosk_회원확인 템플릿 파일 경로 지정
+    return render(request, 'kiosk/kiosk_member.html')  # kiosk_회원확인 템플릿 파일 경로 지정
+
+def point(request):
+    return render(request, 'kiosk/kiosk_point.html')  # kiosk_포인트 적립 템플릿 파일 경로 지정
+
+def usepoint(request):
+    return render(request, 'kiosk/kiosk_usepoint.html')  # kiosk_포인트 사용 템플릿 파일 경로 지정
+
+def payment_method(request):
+    return render(request, 'kiosk/kiosk_payment_method.html')  # kiosk_결제방식 템플릿 파일 경로 지정
+
 
