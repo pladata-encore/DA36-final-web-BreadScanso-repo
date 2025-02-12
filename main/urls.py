3a34af03576aba7ac2210444a5f89a42d3f970ed
jjungah <negmhi29@gmail.com>
2025-02-12 17:46:39 +0900

    Feat: 로그인/회원가입 관련 추가 파일

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from main import views
+from django.contrib.auth import views as auth_views
+
+# namespace 지정
+app_name = 'main'
+
+urlpatterns = [
+    path("login/", auth_views.LoginView.as_view(template_name="main/login.html"), name="login"),  # 로그인 경로
+    path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),  # 로그아웃 경로
+    path("signup/", views.signup, name="signup"),  # 회원가입 경로
+    path("signup/", views.signup, name="user_id"),
+]
---
+++ static/js/signup.js
@@ -0,0 +1,7 @@
+function setAge(age) {
+    document.getElementById("ageDropdownBtn").textContent = age;
+}
+
+function setGender(gender) {
+    document.getElementById("genderDropdownBtn").textContent = gender;
+}
