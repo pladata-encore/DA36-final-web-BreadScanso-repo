8f3717a2aa141970789ce145b2b5085b4ed75449
playdata\playdata2 <yejin3388@naver.com>
2025-02-13 20:22:24 +0900

    Update: kiosk, member 테이블 생성

--- member/models.py
+++ member/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    member_id = models.CharField(max_length=12, primary_key=True)  # 회원 ID (PK)
+    name = models.CharField(max_length=50)
+
