3db567b0ab4f28a586d9e77d19d573f64fb9d15f
playdata\playdata2 <yejin3388@naver.com>
2025-02-14 10:07:15 +0900

    Remove: models.py 코드 삭제

--- pay/models.py
+++ pay/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Purchase(models.Model):
+    purchase_id = models.AutoField(primary_key=True)
+    item_name = models.CharField(max_length=100)
+    price = models.IntegerField()
