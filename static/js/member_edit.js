e68639a98042b2060b32f4601603a267977c80f3
jjungah <negmhi29@gmail.com>
2025-02-14 16:31:07 +0900

    Update: member 점주/회원 관련 파일 업데이트

---
+++ static/js/member_edit.js
@@ -0,0 +1,9 @@
+// 연령대
+function setAge(age) {
+    document.getElementById("ageDropdownBtn").textContent = age;
+}
+
+// 성별
+function setSex(sex) {
+    document.getElementById("sexDropdownBtn").textContent = sex;
+}
