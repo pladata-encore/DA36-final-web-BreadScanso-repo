868252d5406a59355bc647ff5b0b26c98b48315b
jsryan91 <shim0716@gmail.com>
2025-02-17 15:02:27 +0900

    Feat: 제품정고 (재고, 재료) url 생성 및 base_store layout 링크 설정

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
 
 def stock_main(request):
-    return render(request, '../templates/stock/stock_main.html')  # 템플릿 파일 경로 지정
+    return render(request, 'stock/stock_main.html')  # 메인
+
+def stock_product(request):
+    return render(request, 'stock/stock_product.html')  # 제품
+
+def stock_product_edit(request):
+    return render(request, 'stock/stock_product_edit.html')  # 제품 수정
+
+def stock_product_new(request):
+    return render(request, 'stock/stock_product_new.html')  # 제품 신규등록
+
+def stock_ingredient(request):
+    return render(request, 'stock/stock_ingredient.html')  # 재료
+
+def stock_ingredient_edit(request):
+    return render(request, 'stock/stock_ingredient_edit.html')  # 재료 수정
+
+def stock_ingredient_new(request):
+    return render(request, 'stock/stock_ingredient_new.html')  # 재료 신규등록
