4de61013577fb2f0f477bd05d926e3bfe21bd451
jovytree <jovytree@gmail.com>
2025-02-17 12:03:56 +0900

    Feat: 운영 DB 환경속성/변수값 입력, www 도메인주소 추가, dockerrun 환경변수 추가

--- breadscanso/settings_prod.py
+++ breadscanso/settings_prod.py
@@ -1,21 +1,23 @@
 # EB(EC2) 운영환경
 from .settings import *
+import os
 
 # 운영환경에서는 반드시 False로 설정
 DEBUG = True
 
 ALLOWED_HOSTS = [
   'eb-breadscanso-env.eba-ivnsims9.ap-northeast-2.elasticbeanstalk.com',
-  'breadscanso.shop'
+  'breadscanso.shop',
+    'www.breadsanso.shop',
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'PROD_NAME',
-        'USER': 'PROD_USER',
-        'PASSWORD': 'PROD_PASSWORD',
-        'HOST': 'PROD_HOST',
-        'PORT': 'PROD_PORT',
+        'NAME': os.getenv('DATABASE_PROD_NAME'),
+        'USER': os.getenv('DATABASE_PROD_USER'),
+        'PASSWORD': os.getenv('DATABASE_PROD_PASSWORD'),
+        'HOST': os.getenv('DATABASE_PROD_HOST'),
+        'PORT': os.getenv('DATABASE_PROD_PORT'),
     }
 }
