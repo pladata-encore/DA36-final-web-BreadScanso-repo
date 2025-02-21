2fcb6e799217691a976db16951d30dbcce88e835
srogsrogi <han970112@gmail.com>
2025-02-21 17:34:04 +0900

    Fix : notice 관련 model을 member -> notice로 이동

--- member/models.py
+++ member/models.py
@@ -30,20 +30,6 @@ class Member(models.Model):
         return f"회원 {self.member_id} - {self.name}"
 
 
-class Notice(models.Model):  # 공지사항 글 테이블
-    notice_id = models.AutoField(primary_key=True)  # 공지사항 아이디 (PK)
-    title = models.CharField(max_length=30)  # 제목
-    member = models.ForeignKey('Member', on_delete=models.SET_NULL, null=True)  # 회원 아이디 (FK)
-    created_at = models.DateTimeField(auto_now_add=True)  # 등록일시
-    updated_at = models.DateTimeField(auto_now=True)  # 수정일시
-    content = models.TextField()  # 내용
-    view_count = models.IntegerField(default=0)  # 조회수
-    pinned = models.BooleanField(default=False)  # 상단 고정 여부
-    store = models.CharField(max_length=50, null=True, choices=[("A", "Store A"), ("B", "Store B")])  # 매장
-
-    def __str__(self):
-        return f"공지사항 {self.notice_id} - {self.title}"
-
 
 class EventPost(models.Model):  # 이벤트 게시판 글 테이블
     event_id = models.AutoField(primary_key=True)  # 이벤트 게시글 아이디 (PK)
