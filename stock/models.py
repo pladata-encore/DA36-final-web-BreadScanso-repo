42e1b9951dd720788fd0782b099df6185115e78e
minmaru99 <minhaminha5492@gmai.com>
2025-02-21 17:09:31 +0900

    Update: 재고-제품 페이지 추가/삭제 제거, 검색창 style 변경,  재고-재료 페이지 테이블 추가

--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Ingredient(models.Model):
+    ingredient_id=models.AutoField(primary_key=True)
+    ingredient_name = models.CharField(max_length=50)
+    store = models.CharField(max_length=50, choices=[("A", "Store A"), ("B", "Store B")])
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Item {self.ingredient_id}, {self.ingredient_name}"
