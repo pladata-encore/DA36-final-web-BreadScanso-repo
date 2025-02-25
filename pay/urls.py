9fad78f863d8c3fd4ce65d3dc25c6ba55e1afe4e
playdata\playdata2 <yejin3388@naver.com>
2025-02-25 15:30:41 +0900

    Update: layout 적용(마이페이지 메인, 회원정보수정), 마이페이지 화면흐름, 헤더 아이디노출

--- pay/urls.py
+++ pay/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views  # views.py에서 함수 가져오기
 
+# namespace 지정
+app_name = 'pay'
+
 urlpatterns = [
     path('', views.pay_main, name='pay_main'),  # /pay/
 
