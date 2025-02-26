6bdff4fddc14826544fa0b1b7838cf6e9de2baa5
jovytree <jovytree@gmail.com>
2025-02-27 00:35:33 +0900

    Feat: 매장페이지 - 결제관리에 kiosk 주문/결제정보 반영

--- pay/views.py
+++ pay/views.py
@@ -1,29 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from kiosk.models import PaymentInfo
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def kiosk_main(request):
     return render(request, 'kiosk/kiosk_main.html')  # kiosk_main 템플릿 파일 경로 지정
 
+
 def pay_main(request):
-    # GET 요청 처리 (member 데이터 가져오기)
     member = request.user.member
 
-    return render(request, "pay/pay_main.html", {"member": member})
+    # 로그인한 사용자의 store 값 가져오기
+    store = member.store if member.member_type == "manager" else None
+
+    # 해당 store의 결제 내역 가져오기 (store가 없으면 빈 쿼리셋 반환)
+    if store:
+        payment_infos = PaymentInfo.objects.filter(order__store=store).order_by("-pay_at")
+    else:
+        payment_infos = PaymentInfo.objects.none()  # 안전하게 빈 쿼리셋 반환
+
+    # 페이지네이션 (10개씩 표시)
+    paginator = Paginator(payment_infos, 10)  # 한 페이지당 10개씩
+    page_number = request.GET.get("page")  # 현재 페이지 번호 가져오기
+
+    try:
+        page_obj = paginator.get_page(page_number)
+    except (PageNotAnInteger, EmptyPage):
+        page_obj = paginator.get_page(1)  # 유효하지 않은 페이지 번호라면 첫 페이지로 이동
+
+    return render(request, "pay/pay_main.html", {"member": member, "page_obj": page_obj})
 
 
 # def pay_details(request, payment_id):
-def pay_details(request):
-    # payment = get_object_or_404(Purchase, id=payment_id)
-    return render(request, 'pay/pay_details.html')
-    # return render(request, 'pay/pay_details.html', {'payment': payment})
+# def pay_details(request):
+#     # payment = get_object_or_404(Purchase, id=payment_id)
+#     return render(request, 'pay/pay_details.html')
+#     # return render(request, 'pay/pay_details.html', {'payment': payment})
 
+def pay_details(request, payment_id):
+    payment = get_object_or_404(PaymentInfo, payment_id=payment_id)
+    return render(request, 'pay/pay_details.html', {'payment': payment})
+
+
+
+
+# def pay_cancel(request):
+#     # cancels = Purchase.objects.filter(is_cancelled=True).order_by("-date")
+#     # GET 요청 처리 (member 데이터 가져오기)
+#     member = request.user.member
+#
+#     return render(request, 'pay/pay_cancel.html', {"member": member})
+#     # return render(request, "pay/pay_cancel.html", {"cancels": cancels})
 
 def pay_cancel(request):
-    # cancels = Purchase.objects.filter(is_cancelled=True).order_by("-date")
-    # GET 요청 처리 (member 데이터 가져오기)
     member = request.user.member
+    store = member.store if member.member_type == "manager" else None
 
-    return render(request, 'pay/pay_cancel.html', {"member": member})
-    # return render(request, "pay/pay_cancel.html", {"cancels": cancels})
+    # 취소된 결제 내역 가져오기 (payment_status=False)
+    if store:
+        canceled_payments = PaymentInfo.objects.filter(order__store=store, payment_status=False).order_by("-pay_at")
+    else:
+        canceled_payments = []
+
+    return render(request, "pay/pay_cancel.html", {"member": member, "cancels": canceled_payments})
+
 
 def pay_member(request):
     # GET 요청 처리
