947e6b0652d2433651106543181f1be8f17282e7
minmaru99 <minhaminha5492@gmai.com>
2025-02-27 16:39:43 +0900

    Feat: notice 모델 생성

--- notice/models.py
+++ notice/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from member.models import Member
+
 # Create your models here.
 class Notice(models.Model):  # 공지사항 글 테이블
     notice_id = models.AutoField(primary_key=True)  # 공지사항 아이디 (PK)
@@ -10,7 +11,7 @@ class Notice(models.Model):  # 공지사항 글 테이블
     content = models.TextField()  # 내용
     view_count = models.IntegerField(default=0)  # 조회수
     pinned = models.BooleanField(default=False)  # 상단 고정 여부
-    store = models.CharField(max_length=50, null=True, choices=[("A", "Store A"), ("B", "Store B")])  # 매장
+    store = models.CharField(max_length=50, null=True, choices=[("A", "서초점"), ("B", "강남점")])  # 매장
 
     def __str__(self):
         return f"공지사항 {self.notice_id} - {self.title}"
