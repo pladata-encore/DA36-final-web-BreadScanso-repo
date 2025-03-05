9e58ff29425a14d23e9986e73be2c353338b34a0
jjungah <negmhi29@gmail.com>
2025-03-05 11:14:47 +0900

    Update: 소셜로그인 db 연결(auth_user, member_member)

--- member/apps.py
+++ member/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class MemberConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'member'
+
+    def ready(self):
+        import member.signals
+
+
