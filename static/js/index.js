57b79183f68e12936abf43104a9413adf1f67abf
jjungah <negmhi29@gmail.com>
2025-03-10 12:44:59 +0900

    Feat: 시스템설정 - 이벤트 기능(목록, 검색, 삭제, 신규등록)

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    let messageBox = document.getElementById("signup-message");
+    if (messageBox) {
+        messageBox.style.transition = "opacity 0.5s";
+        messageBox.style.opacity = "0";
+        setTimeout(() => messageBox.remove(), 500);
+    }
+}, 5000); // 5초 나타나고 사라지게 했음
