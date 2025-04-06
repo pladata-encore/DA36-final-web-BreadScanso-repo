4f38c0079278d4267e6cc436533cb13856f140c1
jsryan91 <shim0716@gmail.com>
2025-04-06 22:04:32 +0900

    feat: 매장 & 카테고리 매핑을 위한 menu_tags 추가 / menu main 페이지 카드 이미지 크기 수정

--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -1,22 +1,37 @@
-# 이후 매장 혹은 카테고리 추가될때 사용할 수 있도록 store_to_korean, category_to_korean 필터 추가
 from django import template
 
 register = template.Library()
 
+# 매장 매핑 데이터 (새로운 매장은 여기만 추가)
+STORE_MAP = {
+    "A": "서초점",
+    "B": "강남점",
+    # "C": "홍대점",  # 예시로 주석 처리된 매장
+}
+
+# 카테고리 매핑 데이터 (새로운 카테고리는 여기만 추가)
+CATEGORY_MAP = {
+    "bread": "빵",
+    "dessert": "디저트",
+    # "cake": "케이크",  # 예시로 주석 처리된 카테고리
+}
+
+# 필터: 개별 매장 코드를 한국어로 변환
 @register.filter
 def store_to_korean(value):
-    store_map = {
-        "A": "서초점",
-        "B": "강남점",
-        # "C": "홍대점",
-    }
-    return store_map.get(value, value)
+    return STORE_MAP.get(value, value)
 
+# 필터: 개별 카테고리 코드를 한국어로 변환
 @register.filter
 def category_to_korean(value):
-    category_map = {
-        "bread": "빵",
-        "dessert": "디저트",
-        # "cake": "케이크",
-    }
-    return category_map.get(value, value)
+    return CATEGORY_MAP.get(value, value)
+
+# 커스텀 태그: 전체 매장 목록 반환
+@register.simple_tag
+def get_store_map():
+    return STORE_MAP
+
+# 커스텀 태그: 전체 카테고리 목록 반환
+@register.simple_tag
+def get_category_map():
+    return CATEGORY_MAP
